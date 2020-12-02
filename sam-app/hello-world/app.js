9943c2f15fccc1369b203d8c4fe15ba66c69c759
buntafujikawa <bunta.fujikawa@gmail.com>
2020-12-02 21:03:09 +0900

    add sample code.

--- sam-app/hello-world/app.js
+++ sam-app/hello-world/app.js
@@ -7,12 +7,12 @@ let response;
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
     try {
@@ -21,6 +21,7 @@ exports.lambdaHandler = async (event, context) => {
             'statusCode': 200,
             'body': JSON.stringify({
                 message: 'hello world',
+                message2: 'hello world2',
                 // location: ret.data.trim()
             })
         }
